d144289755d1055c0b416f7f53e34e247cdb5839
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-09 14:55:51 -0300

    sm7325-common: Disable ART debug and optimize dexpreopt
     * minimize java debug info
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * disable debug configs in art
     * use non-debug dex2oat

--- sm7325.mk
+++ sm7325.mk
@@ -157,6 +157,12 @@ PRODUCT_PACKAGES += \
 # Charger
 WITH_LINEAGE_CHARGER := false
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
